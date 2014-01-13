8fe525e4f14cfe6711764b62c0edb05ad6d809f9
Alexey Korolev <akorolex@gmail.com>
2014-01-13 17:10:31 +1300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SovpokAuth::Application.config.secret_key_base = 'c1bdf97d93f92f5168b8d260f2a415585055f3e2c4d117d18f9cac8b2e85f521bf697a7aaea954ee4f1dbd20dcd5c7c4ae7465b358d9965d39bc3273ac96288b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SovpokAuth::Application.config.session_store :cookie_store, key: '_sovpok_auth_session'
