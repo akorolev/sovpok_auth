ca7d171060f4cd856f3993074134f1a3c073742b
Alexey Korolev <akorolex@gmail.com>
2014-02-27 18:05:15 +1300

    Fixed seeds file for init. P/S styling is screwed a bit now. Probably because of foundation update.
    Signed-off-by: Alexey Korolev <akorolex@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,16 @@ YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name(role)
   puts 'role: ' << role
 end
+
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+user = User.find_or_create_by(:email => ENV['ADMIN_EMAIL'].dup)
+user.password = ENV['ADMIN_PASSWORD'].dup
+user.password_confirmation = ENV['ADMIN_PASSWORD'].dup
+
+profile = Profile.find_or_create_by(:user_id=>user.id)
+profile.name = ENV['ADMIN_NAME'].dup
+
 #user.confirm!
 user.add_role :admin
+user.save
+profile.save
