1eddf2769097978d6c169377ad625a22cd9d536f
Alexey Korolev <akorolex@gmail.com>
2014-02-28 15:18:50 +1300

    Corrected routes a bit Signed-off-by: Alexey Korolev <akorolex@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SovpokAuth::Application.routes.draw do
-  get "profiles/edit"
-  put "profiles/update"
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions",:omniauth_callbacks => "omniauth_callbacks"}
-  resources :users, :profiles
+  resources :users
+  get "profiles/edit"
+  put "profiles/update"
+#  resources :profiles, :only => [:edit]
 end
