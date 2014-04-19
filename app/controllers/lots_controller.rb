6118c7a6029639a6c96336e5fff3fe73ea58f33d
alex <alex@homedesk.(none)>
2014-04-19 15:49:23 +1200

    Added validation, not complete yet
    Signed-off-by:  <akorolex>

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -25,7 +25,7 @@ class LotsController < ApplicationController
   # POST /lots.json
   def create
     @lot = Lot.new(lot_params)
-
+    puts lot_params.inspect
     respond_to do |format|
       if @lot.save
         format.html { redirect_to @lot, notice: 'Lot was successfully created.' }
@@ -69,6 +69,13 @@ class LotsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def lot_params
-      params.require(:lot).permit(:title, :description, :end_date, :prepay, :fee, :min_order, :pickup_allowed, :pickup_description, :postage_val1, :postage_info1, :postage_val2, :postage_info2, :postage_val3, :postage_info3, :source, :serial, :keywords, :status, :category_id, :user_id, :terms_of_service)
+      ret = params.require(:lot).permit(:title, :description, :end_date, :prepay, :fee, :min_order, :pickup_allowed, :pickup_description, :postage_val1, :postage_info1, :postage_val2, :postage_info2, :postage_val3, :postage_info3, :source, :serial, :keywords, :status, :category_id, :user_id, :condition,:terms_of_service)
+      for i in 1..3
+        ret.update(params.require(:lot).permit("postage_val#{i}", "postage_info#{i}"))
+      end
+      for i in 0..CategoriesController::LVL_MASK.length
+        ret.update(params.require(:lot).permit("sub_category_lvl#{i}"))
+      end
+      ret
     end
 end
--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,7 +1,27 @@
 class Lot < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { minimum: 15, maximum: 1000 }
+  validates :end_date, presence: true
+  validates :prepay, numericality: { only_integer: true }, allow_blank: true
+  validates :fee, numericality: { only_integer: true }, allow_blank: true
+  validates :min_order, numericality: true, allow_blank: true
+  validates :pickup_description, length: { maximum: 1000 }, allow_blank: true
+  validates :postage_val1, numericality: true, allow_blank: true
+  validates :postage_info1, length: { maximum: 50 }, allow_blank: true
+  validates :postage_val2, numericality: true, allow_blank: true
+  validates :postage_info2, length: { maximum: 50 }, allow_blank: true
+  validates :postage_val3, numericality: true, allow_blank: true
+  validates :postage_info3, length: { maximum: 50 }, allow_blank: true
+  validates :source, length: { maximum: 50 }, allow_blank: true
+
+  validates :category_id, presence: true
+
+  validates :condition, presence: true, length: { minimum: 15, maximum: 1000 }
   validates :terms_of_service, acceptance: true
+
 
   for lvl in 0..CategoriesController::LVL_MASK.length do 
     define_method "sub_category_lvl#{lvl}" do
